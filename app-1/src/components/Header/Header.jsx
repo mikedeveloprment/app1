62a47b2137e26d877f3438dc3eddbfda48766736
mikedeveloprment <mikebahgneiy@gmail.com>
2023-06-22 01:32:59 +0300

    dvda

--- app-1/src/components/Header/Header.jsx
+++ app-1/src/components/Header/Header.jsx
@@ -1,23 +1,41 @@
-import React from 'react'
-import clas  from "./Header.module.scss";
-import { SmoothCorners } from 'react-smooth-corners'
-
+import React from "react";
+import clas from "./Header.module.scss";
 
 const Header = () => {
-  return (
-    <header className={clas.header}>
-    <div className={clas.logo}>Host</div>
-    <button className={clas.menu}>
-      <div className={clas.iconContainer}>
-        <span className={clas.circle}></span>
-        <span className={clas.circle}></span>
-        <span className={clas.circle}></span>
-        <span className={clas.circle}></span>
-      </div>
-    </button>
+	const [menuActiveClass, setMenuActiveClass] = React.useState(false);
+	console.log(menuActiveClass);
+	return (
+		<header className={clas.header}>
+			<div className={clas.logo}>Host</div>
+			<button
+				onClick={() => setMenuActiveClass(!menuActiveClass)}
+        className={
+						menuActiveClass ? clas.menu : ` ${clas.menu} ${clas.menuActive}`
+					}
+			>
+				<div className={clas.iconContainer}>
+					{[...Array(4)].map((_, i) => (
+						<span
+							style={{
+								transitionDelay: `${menuActiveClass? (i * 0.1 + 0.5) : (i * 0.1)}s`,
+							}}
+							key={i}
+							className={
+								menuActiveClass
+									? clas.circle
+									: ` ${clas.circle} ${clas.circleActive}`
+							}
+						></span>
+					))}
+				</div>
+        <nav
+					className={
+						menuActiveClass ? clas.nav : ` ${clas.nav} ${clas.navActive}`
+					}
+				></nav>
+			</button>
+		</header>
+	);
+};
 
-    </header>
-  )
-}
-
-export default Header
+export default Header;
