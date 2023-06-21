f53eb5e773142499940d9868b8bb53412cb94f9a
mikedeveloprment <mikebahgneiy@gmail.com>
2023-06-22 02:22:27 +0300

    dkjvbhjs

--- app-1/src/components/Header/Header.jsx
+++ app-1/src/components/Header/Header.jsx
@@ -2,22 +2,32 @@ import React from "react";
 import clas from "./Header.module.scss";
 
 const Header = () => {
-	const [menuActiveClass, setMenuActiveClass] = React.useState(false);
-	console.log(menuActiveClass);
+	const [menuActiveClass, setMenuActiveClass] = React.useState(true);
+	const word = "Host";
+	const textArray = word.split("");
+	console.log(textArray);
 	return (
 		<header className={clas.header}>
-			<div className={clas.logo}>Host</div>
+			<div className={clas.logo}>
+				{textArray.map((letter, index) => (
+					<span key={index} style={{
+            animationDelay: `${index*0.1}s`
+          }} className={clas.letter}>{letter}</span>
+				))}
+			</div>
 			<button
 				onClick={() => setMenuActiveClass(!menuActiveClass)}
-        className={
-						menuActiveClass ? clas.menu : ` ${clas.menu} ${clas.menuActive}`
-					}
+				className={
+					menuActiveClass ? clas.menu : ` ${clas.menu} ${clas.menuActive}`
+				}
 			>
 				<div className={clas.iconContainer}>
 					{[...Array(4)].map((_, i) => (
 						<span
 							style={{
-								transitionDelay: `${menuActiveClass? (i * 0.1 + 0.5) : (i * 0.1)}s`,
+								transitionDelay: `${
+									menuActiveClass ? i * 0.1 + 0.5 : i * 0.1
+								}s`,
 							}}
 							key={i}
 							className={
@@ -28,7 +38,7 @@ const Header = () => {
 						></span>
 					))}
 				</div>
-        <nav
+				<nav
 					className={
 						menuActiveClass ? clas.nav : ` ${clas.nav} ${clas.navActive}`
 					}
