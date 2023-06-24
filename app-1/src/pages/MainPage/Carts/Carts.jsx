48abdbea6e1f4d71861710548e148e12b777238e
mikedeveloprment <mikebahgneiy@gmail.com>
2023-06-24 12:01:40 +0300

    fix

--- app-1/src/pages/MainPage/Carts/Carts.jsx
+++ app-1/src/pages/MainPage/Carts/Carts.jsx
@@ -1,38 +1,17 @@
 import React from "react";
 import clas from "./Carts.module.scss";
 import { motion } from "framer-motion";
-import { SmoothCorners } from 'react-smooth-corners'
 
 const Carts = () => {
 	return (
 		<div className={clas.cartsContainer}>
-			<SmoothCorners
-				corners="18, 9"
-				as="div"
-				borderRadius="18px"
-				className={`${clas.cartControll} ${clas.cart}`}
-			></SmoothCorners>
+			<div className={`${clas.cartControll} ${clas.cart}`}></div>
 			<div className={clas.cartsContainer2}>
 				<div className={`${clas.cartsContainer3}`}>
-					<SmoothCorners
-						corners="18,9"
-						as="div"
-						borderRadius="18px"
-						className={`${clas.cartControll2} ${clas.cart}`}
-					></SmoothCorners>
-					<SmoothCorners
-						corners="18,9"
-						as="div"
-						borderRadius="18px"
-						className={`${clas.cartControll3} ${clas.cart}`}
-					></SmoothCorners>
+				<div className={`${clas.cartControll2} ${clas.cart}`}></div>
+				<div className={`${clas.cartControll3} ${clas.cart}`}></div>
 				</div>
-				<SmoothCorners
-					corners="4, 22"
-					as="div"
-					borderRadius="18px"
-					className={`${clas.cartControll4} ${clas.cart}`}
-				></SmoothCorners>
+				<div className={`${clas.cartControll4} ${clas.cart}`}></div>
 			</div>
 		</div>
 	);
