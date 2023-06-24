6904f43d93c7f9aee471d5360c8fb5df97f030a3
mikedeveloprment <mikebahgneiy@gmail.com>
2023-06-24 12:44:51 +0300

    fix

--- app-1/src/pages/MainPage/Carts/Cart1/Cart1.jsx
+++ app-1/src/pages/MainPage/Carts/Cart1/Cart1.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
 import clas from "./Cart1.module.scss";
 const Cart1 = () => {
-  return (
-    <div className={`${clas.cartControll} ${clas.cart}`}>
-				d
-    </div>
-  )
-}
+	const [checkActive, setcheckActive] = React.useState(false);
+	return (
+		<div className={`${clas.cartControll} ${clas.cart}`}>
+			<div className={clas.infoCont}>
+				<p className={clas.textCard}>Temperature</p>
+				<button
+					onClick={() => setcheckActive(!checkActive)}
+					className={
+						checkActive
+							? clas.checkboks
+							: `${clas.checkboks} ${clas.checkboksActive}`
+					}
+				>
+					<span
+						className={
+							checkActive ? clas.circle : `${clas.circle} ${clas.circleActive}`
+						}
+					></span>
+				</button>
+			</div>
+		</div>
+	);
+};
 
-export default Cart1
+export default Cart1;
